e5cd802bc60f96a89673381d0b08d754eaa1a890
Siyu <jiansiyu@gmail.com>
2021-03-26 01:15:43 -0400

    add new dataset

--- dataGenerator/getData.C
+++ dataGenerator/getData.C
@@ -110,13 +110,222 @@ std::map<TString,double> getCombination(double  x, double theta,double y, double
         Int_t phIndex =  title[9]-'0';
         res[str] = pow(x,xIndex)*pow(theta,thIndex)*pow(y,yIndex)*pow(phi,phIndex);
 
-//        std::cout<<str.Data()<<" :: "<<res[str] <<"  "<<xIndex<<","<<thIndex<<","<<yIndex<<","<<phIndex<<std::endl;
+//std::cout<<str.Data()<<" :: "<<res[str] <<"  "<<xIndex<<","<<thIndex<<","<<yIndex<<","<<phIndex<<std::endl;
     }
     return  res;
 }
 
 
+//_______________________________________________________________________________________________________
+void GetMinSieveY(TString fnameTemplate="./data/data_y_all/CheckVertex_Report_%d.root",Int_t sieveMinCT = 100){
+    Int_t runList[]={2239,2240,2241,2244,2245,2256,2257};
 
+    std::vector<Int_t> SieveEvtCT;
+    std::map<Int_t,std::set<Int_t>> cutIDBuff;
+
+    for (Int_t runIndex = 0; runIndex < sizeof(runList)/sizeof(Int_t); runIndex++){
+        auto runID = runList[runIndex];
+        // load the file and connect the data, try to get the number of the sieveholes
+        std::set<Int_t> cutIDSet;
+        TString filename = Form(fnameTemplate.Data(),runID);
+
+        TChain *chain = new TChain("OptRes");
+        chain->Add(filename.Data());
+
+        // get the number of the cutID
+        // map the data
+
+        int evtID;
+        int CutID;
+        int SieveRowID;
+        int SieveColID;
+        int KineID;
+
+        double bpmX;     // the bpm coordination sys
+        double bpmY;
+        double bpmZ;
+
+        double focalTh;   //the focal plane variable
+        double focalPh;
+        double focalX;
+        double focalY;
+
+
+        double tfiletargCalcY;
+        double tfiletargProjY;
+
+
+
+        chain->SetBranchAddress("evtID",&evtID);
+        chain->SetBranchAddress("CutID",&CutID);
+        chain->SetBranchAddress("SieveRowID",&SieveRowID);
+        chain->SetBranchAddress("SieveColID",&SieveColID);
+        chain->SetBranchAddress("KineID",&KineID);
+
+        chain->SetBranchAddress("focalTh",&focalTh);
+        chain->SetBranchAddress("focalPh",&focalPh);
+        chain->SetBranchAddress("focalX",&focalX);
+        chain->SetBranchAddress("focalY",&focalY);
+        // get the beam position
+        chain->SetBranchAddress("bpmX",&bpmX);
+        chain->SetBranchAddress("bpmY",&bpmY);
+
+
+        chain->SetBranchAddress("tfiletargCalcY",&tfiletargCalcY);
+        chain->SetBranchAddress("tfiletargProjY",&tfiletargProjY);
+
+
+
+        for(Long64_t i = 0 ; i < chain->GetEntries(); i ++){
+            chain->GetEntry(i);
+            //TODO apply cut on the sieve holes
+            if (SieveHoleCut(SieveColID,SieveRowID))
+                cutIDSet.insert(CutID);
+        }
+
+        //Get the event number
+        for (std::set<Int_t>::iterator iter = cutIDSet.begin(); iter != cutIDSet.end(); iter++){
+            auto cutid = *iter;
+            // get the entries
+            auto evtCT = chain->GetEntries(Form("CutID == %d",cutid));
+            SieveEvtCT.push_back(evtCT);
+        }
+
+        cutIDBuff[runID] = cutIDSet;
+
+    }
+
+    std::cout<<*std::min_element(SieveEvtCT.begin(),SieveEvtCT.end()) <<std::endl;
+
+    auto SieveEvtMin = *std::min_element(SieveEvtCT.begin(),SieveEvtCT.end());
+
+    // loop on the files
+    if(sieveMinCT <= 0) sieveMinCT  = SieveEvtMin;
+
+    for (Int_t runIndex = 0; runIndex < sizeof(runList)/sizeof(Int_t); runIndex++) {
+        auto runID = runList[runIndex];
+        // load the file and connect the data, try to get the number of the sieveholes
+        TString filename = Form(fnameTemplate.Data(), runID);
+
+        TChain *chain = new TChain("OptRes");
+        chain->Add(filename.Data());
+
+        // get the number of the cutID
+        // map the data
+        int evtID;
+        int CutID;
+        int SieveRowID;
+        int SieveColID;
+        int KineID;
+
+        double bpmX;     // the bpm coordination sys
+        double bpmY;
+        double bpmZ;
+
+        double focalTh;   //the focal plane variable
+        double focalPh;
+        double focalX;
+        double focalY;
+
+        //double TargCalTh;  // the calculated theoretical Value on the target coordination sys
+        //double TargCalPh;
+
+        double tfiletargCalcY;
+        double tfiletargProjY;
+
+        chain->SetBranchAddress("evtID", &evtID);
+        chain->SetBranchAddress("CutID", &CutID);
+        chain->SetBranchAddress("SieveRowID", &SieveRowID);
+        chain->SetBranchAddress("SieveColID", &SieveColID);
+        chain->SetBranchAddress("KineID", &KineID);
+
+        chain->SetBranchAddress("focalTh", &focalTh);
+        chain->SetBranchAddress("focalPh", &focalPh);
+        chain->SetBranchAddress("focalX", &focalX);
+        chain->SetBranchAddress("focalY", &focalY);
+        // get the beam position
+        chain->SetBranchAddress("bpmX", &bpmX);
+        chain->SetBranchAddress("bpmY", &bpmY);
+
+        chain->SetBranchAddress("tfiletargCalcY",&tfiletargCalcY);
+        chain->SetBranchAddress("tfiletargProjY",&tfiletargProjY);
+
+        std::cout <<"Working on "<<runID<<std::endl;
+        {
+            //loop on the event and write the data to csv file
+            std::map<Int_t, Int_t> SieveEvtTracker;   //# sieve event tracker
+            Int_t SieveTotalConter = 0;
+            for (std::set<Int_t>::iterator iter = cutIDBuff[runID].begin(); iter != cutIDBuff[runID].end(); iter++) {
+                auto cutid = *iter;
+                SieveEvtTracker[cutid] = 0;
+            }
+
+            std::ofstream csvfileIO;
+            csvfileIO.open(Form("./result/regY/PRex_DataSet_Vertex_%d.csv", runID));
+            csvfileIO << "evtID,runID,CutID,SieveRowID,SieveColID,bpmX,bpmY,focal_x,focal_y,focal_th,focal_ph,tfiletargCalcY,tfiletargProjY\n";
+            Long64_t entries = chain->GetEntries();
+            for (Long64_t entry = 0; entry < entries; entry++) {
+                // TODO need to change to random access the entry
+                chain->GetEntry(entry);
+                if (SieveEvtTracker[CutID] < sieveMinCT) {
+                    SieveEvtTracker[CutID] += 1;
+                    SieveTotalConter += 1;
+                    // write the data to the csv files
+                    csvfileIO << Form("%d,%d,%d,%d,%d,%f,%f,%f,%f,%f,%f,%f,%f \n", evtID,runID,CutID,SieveRowID,SieveColID,bpmX, bpmY, focalX, focalY,
+                                      focalTh, focalPh, tfiletargCalcY, tfiletargProjY);
+
+                }
+
+                if (SieveTotalConter >= SieveEvtTracker.size() * sieveMinCT) break;
+            }
+            csvfileIO.close();
+        }
+
+        {
+            //loop on the event and write the data to csv file
+            std::map<Int_t,Int_t> SieveEvtTracker;   //# sieve event tracker
+            Int_t SieveTotalConter=0;
+            for (std::set<Int_t>::iterator iter = cutIDBuff[runID].begin(); iter != cutIDBuff[runID].end(); iter++){
+                auto cutid = *iter;
+                SieveEvtTracker[cutid] = 0;
+            }
+            std::ofstream csvFullfileIO; // include all the combinations
+            csvFullfileIO.open(Form("./result/regY/PRex_DataSet_Vertex_Full_%d.csv",runID));
+            csvFullfileIO<<"evtID,runID,CutID,SieveRowID,SieveColID,bpmX,bpmY";
+            std::map<TString,double> titleComb = getCombination(0,0,0,0);
+            for (auto iter = titleComb.begin(); iter!=titleComb.end();iter++){
+                TString str = iter->first;
+                csvFullfileIO<<","<<str.Data();
+            }
+            csvFullfileIO<<",tfiletargCalcY,tfiletargProjY\n";
+            Long64_t entries = chain->GetEntries();
+            for (Long64_t entry = 0; entry < entries; entry++) {
+                // TODO need to change to random access the entry
+                chain->GetEntry(entry);
+                if (SieveEvtTracker[CutID] < sieveMinCT) {
+                    SieveEvtTracker[CutID] += 1;
+                    SieveTotalConter += 1;
+                    csvFullfileIO << Form("%d,%d,%d,%d,%d,%f,%f", evtID,runID, CutID,SieveRowID,SieveColID,bpmX, bpmY);
+
+                    auto newComb = getCombination(focalX,focalTh,focalY,focalPh);
+                    for (auto iter = newComb.begin();iter!=newComb.end();iter ++){
+                        csvFullfileIO <<","<<iter->second;
+//                        std::cout<<(iter->first).Data()<<"  : "<<iter->second <<"      (x,th,y,ph):: "<<focalX<<", "<<focalTh<<", "<<focalY<<", "<<focalPh
+//                        <<std::endl;
+                    }
+                    csvFullfileIO<<Form(",%f,%f\n",tfiletargCalcY, tfiletargProjY);
+                }
+
+                if (SieveTotalConter >= SieveEvtTracker.size() * sieveMinCT) break;
+            }
+            csvFullfileIO.close();
+        }
+        chain->Delete();
+    }
+
+}
+
+//_______________________________________________________________________________________________________
 void GetMinSieveEvent(TString fnameTemplate="./data/data_focal/checkSieve_%d.root",Int_t sieveMinCT = 0){
 
     Int_t runList[]={2239,2240,2241,2244,2245,2256,2257};
@@ -398,6 +607,7 @@ void GetMinSieveEvent(TString fnameTemplate="./data/data_focal/checkSieve_%d.roo
 
 int main(){
     std::cout<<"This is test functions"<<std::endl;
-    GetMinSieveEvent();
+    GetMinSieveY();
+//    GetMinSieveEvent();
     return 1;
 }
